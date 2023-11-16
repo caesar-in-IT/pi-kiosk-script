007fcc6b90e411a478b9ec8337cd267c7c1cf6fe
58Foundations <150956876+58Foundations@users.noreply.github.com>
2023-11-16 10:14:56 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,8 @@
 sudo crontab -e
   59 23 * * * /sbin/shutdown -r
   @reboot /usr/bin/env >> /home/helpdesk/cron_output.txt
+  @reboot xset s off
+  @reboot xset -dpms
+  @reboot xset s no blank
+  @reboot unclutter -idle 0.1 -root
+  @reboot chromium-browser --kiosk /home/helpdesk/Desktop/youtube.html
