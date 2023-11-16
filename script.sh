3274ca80602efc124925eaa050d881bef2cd7868
caesar-in-IT <146789200+caesar-in-IT@users.noreply.github.com>
2023-11-16 10:49:09 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,9 @@
 sudo crontab -e
   59 23 * * * /sbin/shutdown -r
+  @reboot git 
   @reboot /usr/bin/env >> /home/helpdesk/cron_output.txt
   @reboot xset s off
   @reboot xset -dpms
   @reboot xset s no blank
   @reboot unclutter -idle 0.1 -root
-  @reboot chromium-browser --kiosk /home/helpdesk/Raspberry-Kiosk-Script/youtube.html
+  @reboot chromium-browser --start-fullscreen --kiosk /home/helpdesk/Raspberry-Kiosk-Script/youtube.html
