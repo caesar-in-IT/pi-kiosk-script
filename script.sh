2d0aa53b2ecf0d60cde3a71e60dcf5ae0d8df849
58Foundations <150956876+58Foundations@users.noreply.github.com>
2023-11-27 16:01:48 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,3 @@
 sudo crontab -e
-  59 23 * * * /sbin/shutdown -r
-  @reboot git 
   @reboot /usr/bin/env >> /home/helpdesk/cron_output.txt
-  @reboot xset s off
-  @reboot xset -dpms
-  @reboot xset s no blank
-  @reboot unclutter -idle 0.1 -root
-  @reboot chromium-browser --start-fullscreen --kiosk /home/helpdesk/Raspberry-Kiosk-Script/youtube.html
+ 
